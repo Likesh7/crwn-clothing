f504cf8f822f6b7674f07e13ff60f679db804fdd
likesh <likeshshrestha@olivemedia.co>
2019-07-24 10:33:52 +0545

    add transition to menu item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,10 +5,11 @@ import "./menu-item.styles.scss";
 const MenuItem = ({ title, imageUrl, size }) => {
   console.log("img", imageUrl);
   return (
-    <div
-      className={`${size} menu-item`}
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">shop now</span>
